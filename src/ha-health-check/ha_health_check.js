d2feef44468fc2b25a726ded68c0b10709f16075
pritishmishra <pritish.mishra@sap.com>
2018-06-13 15:07:55 +0530

    Modifying the logic of ha health check app

--- src/ha-health-check/ha_health_check.js
+++ src/ha-health-check/ha_health_check.js
@@ -1,20 +1,34 @@
 'use strict';
 
 const http = require('http');
-const {
-  spawnSync
-} = require('child_process');
+const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
-  const ls = spawnSync('bash', ['/var/vcap/jobs/keepalived/bin/get_keepalived_state.sh']);
-  if (ls.stdout.toString().trim() === 'up') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Up');
-  } else {
-    res.writeHead(500, { 'Content-Type': 'text/html' });
-    res.write('Down');
-  }
-  res.end();
+  exec('bash /var/vcap/jobs/keepalived/bin/get_keepalived_state.sh', function (error, stdout, stderr) {
+    var status = 500;
+    if (error !== null) {
+      console.log('error', error);
+      if (stderr) {
+        console.log('stderr', stderr);
+      }
+      status = 500;
+    } else {
+      if (stdout !== null && stdout.toString().trim() === 'up') {
+        status = 200;
+      } else {
+        status = 500;
+      }
+    }
+    res.writeHead(status, {
+      'Content-Type': 'text/plain'
+    });
+    if (status === 200) {
+      res.write('Up');
+    } else {
+      res.write('Down');
+    }
+    res.end();
+  });
 }).listen(9595);
 
-console.log(`${Date.now}: Listening on 9595`);
+console.log(`${Date.now()}: Listening on 9595`);
