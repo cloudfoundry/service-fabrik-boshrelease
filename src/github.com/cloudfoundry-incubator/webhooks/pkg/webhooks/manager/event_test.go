fc935d889c93fa2ec1b67462e1321e8b703ac039
Murali Suresh <murali.suresh@sap.com>
2019-01-02 15:57:57 +0530

    Add tests

--- src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/event_test.go
+++ src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/event_test.go
@@ -54,13 +54,62 @@ var _ = Describe("Event", func() {
 		})
 	})
 	Describe("isMeteringEvent", func() {
-		Context("Create", func() {
+		Context("When Type is Update", func() {
+			It("Should should return true if update succeeds", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "update"
+				evt.crd.Status.lastOperation.State = "succeeded"
+				evt.oldCrd.Status.lastOperation.Type = "update"
+				evt.oldCrd.Status.lastOperation.State = "in_progress"
+				Expect(evt.isMeteringEvent()).To(Equal(true))
+			})
+			It("Should should return false if update fails", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "update"
+				evt.crd.Status.lastOperation.State = "failed"
+				evt.oldCrd.Status.lastOperation.Type = "update"
+				evt.oldCrd.Status.lastOperation.State = "in_progress"
+				Expect(evt.isMeteringEvent()).To(Equal(false))
+			})
+		})
+		Context("When Type is Create", func() {
 			It("Should should return true if create succeeds", func() {
 				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "create"
+				evt.crd.Status.lastOperation.State = "succeeded"
+				evt.oldCrd.Status.lastOperation.Type = "create"
+				evt.oldCrd.Status.lastOperation.State = "in_progress"
 				Expect(evt.isMeteringEvent()).To(Equal(true))
+			})
+			It("Should should return true only if create state changes", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "create"
+				evt.crd.Status.lastOperation.State = "succeeded"
+				evt.oldCrd.Status.lastOperation.Type = "create"
+				evt.oldCrd.Status.lastOperation.State = "succeeded"
+				Expect(evt.isMeteringEvent()).To(Equal(false))
+			})
+			It("Should should return false if create fails", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "create"
+				evt.crd.Status.lastOperation.State = "failed"
+				evt.oldCrd.Status.lastOperation.Type = "create"
+				evt.oldCrd.Status.lastOperation.State = "in_progress"
+				Expect(evt.isMeteringEvent()).To(Equal(false))
 			})
 		})
 	})
+
+	Describe("ObjectToMapInterface", func() {
+		It("Should convert object to map", func() {
+			expected := make(map[string]interface{})
+			expected["options"] = "dummyOptions"
+			Expect(ObjectToMapInterface(GenericSpec{
+				Options: "dummyOptions",
+			})).To(Equal(expected))
+		})
+	})
+
 	Describe("getClient", func() {
 		Context("with the passed config", func() {
 			It("Should should return a valid client", func() {
@@ -68,6 +117,28 @@ var _ = Describe("Event", func() {
 			})
 		})
 	})
+
+	Describe("getDocs", func() {
+		Context("when type is update", func() {
+			It("Generates two metering docs", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "update"
+				docs, err := evt.getDocs()
+				Expect(err).To(BeNil())
+				Expect(len(docs)).To(Equal(2))
+			})
+		})
+		Context("when type is create", func() {
+			It("Generates one metering doc", func() {
+				evt, _ := NewEvent(&ar)
+				evt.crd.Status.lastOperation.Type = "create"
+				docs, err := evt.getDocs()
+				Expect(err).To(BeNil())
+				Expect(len(docs)).To(Equal(1))
+			})
+		})
+	})
+
 	Describe("createMetering", func() {
 		Context("with the passed config", func() {
 			It("Should return no kind match error", func() {
