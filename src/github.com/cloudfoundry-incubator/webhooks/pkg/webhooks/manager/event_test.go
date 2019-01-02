fff18c403d1f1ce49519b972acafcca9fffbd7c5
Murali Suresh <murali.suresh@sap.com>
2019-01-02 15:09:35 +0530

    Add tests for NewEvent

--- src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/event_test.go
+++ src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/event_test.go
@@ -34,6 +34,20 @@ var _ = Describe("Event", func() {
 			Expect(evt).ToNot(Equal(nil))
 			Expect(err).To(BeNil())
 		})
+		It("Should throw error if object cannot be parsed" , func(){
+            temp := ar.Request.Object.Raw
+		    ar.Request.Object.Raw = []byte("")
+			evt, err := NewEvent(&ar)
+			Expect(evt).To(BeNil())
+			Expect(err).ToNot(BeNil())
+            ar.Request.Object.Raw = temp
+		})
+		It("Should throw error if old object cannot be parsed" , func(){
+		    ar.Request.OldObject.Raw = []byte("")
+			evt, err := NewEvent(&ar)
+			Expect(evt).To(BeNil())
+			Expect(err).ToNot(BeNil())
+		})
 	})
 	Describe("isMeteringEvent", func() {
 		Context("Create", func() {
@@ -50,7 +64,7 @@ var _ = Describe("Event", func() {
 			})
 		})
 	})
-	Describe("createMertering", func() {
+	Describe("createMetering", func() {
 		Context("with the passed config", func() {
 			It("Should return no kind match error", func() {
 				evt, _ := NewEvent(&ar)
