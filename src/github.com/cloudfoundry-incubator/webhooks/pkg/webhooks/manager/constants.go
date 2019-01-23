54f49532dce796275e2579dfdfb02046d0ed6d58
Murali S <muralisc@gmail.com>
2019-01-23 23:54:51 +0530

    Add comments to exported variables

--- src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/constants.go
+++ src/github.com/cloudfoundry-incubator/webhooks/pkg/webhooks/manager/constants.go
@@ -1,7 +1,10 @@
 package main
 
 const (
-	MeterStart         = 1
-	MeterStop          = 0
+	// MeterStart signals the creation of an instance
+	MeterStart = 1
+	// MeterStop signals the deletion of an instance
+	MeterStop = 0
+	// ToBeMetered is the default state of metering resource
 	ToBeMetered = "TO_BE_METERED"
 )
